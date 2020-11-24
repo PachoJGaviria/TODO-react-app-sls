2ad17f5e2663a08391912913a9b04f02fcf70fdd
Francisco Javier Gaviria Sierra <fgaviria@psl.com.co>
2020-11-23 23:15:33 -0500

    feat: init repo
    add code base

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'rgggxs03oa'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-8gn1qd2h.us.auth0.com',            // Auth0 domain
+  clientId: 'tjkWpO1i0bFUGTi6KN5WhFli2Xaho7DM',   // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
